4d0846a2785cb216580c8be6a9e16d9e081191ca
Sahil Tuladhar <sailratna11@gmail.com>
2025-08-05 07:28:20 +0545

    Initial Setup

---
+++ frontend-client/src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="flex items-center justify-center">
+      <h1 className="border border-amber-300">Note Application</h1>
+    </div>
+  );
+}
+
+export default App;
